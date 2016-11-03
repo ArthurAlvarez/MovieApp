142ac8bc32b387da5edb8a714ae0c031f51aa0b8
Arthur Alvarez <arthuralvarez@Arthurs-MacBook-Pro.local>
2016-11-03 11:10:04 -0200

    Storyboards for Upcoming Movies View Controller

---
+++ MovieApp/UpcomingMovieTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  UpcomingMovieTableViewCell.swift
+//  MovieApp
+//
+//  Created by Arthur Alvarez on 03/11/16.
+//  Copyright © 2016 Arthur Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingMovieTableViewCell: UITableViewCell {
+
+    // MARK: - Storyboard Outlets
+    
+    @IBOutlet weak var backdropImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
---
+++ MovieApp/UpcomingMoviesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingMoviesViewController.swift
+//  MovieApp
+//
+//  Created by Arthur Alvarez on 03/11/16.
+//  Copyright © 2016 Arthur Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingMoviesViewController: UIViewController {
+
+    // MARK: - Storyboard Outlets
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    @IBOutlet weak var upcomingMoviesTableView: UITableView!
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
